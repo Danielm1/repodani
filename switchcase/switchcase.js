913a182e8219efb604637d5db6432684f7527853
Danielm1 <daniel_alfayomega@hotmail.com>
2016-04-21 21:10:57 -0500

    Switchcase

---
+++ switchcase/switchcase.js
@@ -0,0 +1,21 @@
+var dia="Inocentechorejita";
+switch (dia)
+{
+	case "Lunes":
+		alert("Te voy a pellizcar la choreja nusy");
+		break;
+	case "Martes":
+		alert("Siempre pellizcare tu choreja");
+		break;
+	case "Miercoles":
+		alert("Voy a molestar a nusy :D");
+		break;
+	case "Jueves":
+		alert("Nusy Menso");
+		break;
+	case "Viernes":
+		alert("Todos los días pellizcare tu choreja nusy");
+		break;
+	default:
+	 	alert("Es un dia festivo");
+}
